4f0902857b666b948258c7bfbee5bded1dec1d22
Daniel Röpke <daniel.ropke.duarte@gmail.com>
2025-07-04 14:20:28 -0300

    Corrige rota inicial para /login

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,9 @@ function App() {
 
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login onLogin={handleLogin} />} />
+
       {logado ? (
         <>
           <Route path="/home" element={<Home />} />
