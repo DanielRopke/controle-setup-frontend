9d841a3fb1d9c3815c91e4fbff849ae13d2c3a4e
Daniel Röpke <daniel.ropke.duarte@gmail.com>
2025-07-07 15:22:30 -0300

    Mensagem descritiva sobre a atualização

--- src/App.tsx
+++ src/App.tsx
@@ -39,9 +39,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login onLogin={handleLogin} />} />
-
       {logado ? (
         <>
           <Route path="/home" element={<Home />} />
